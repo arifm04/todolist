6b985f57fe3ccfeb8a262cf3e6f5e11eee4dc29b
arifms04 <49025330+arifm04@users.noreply.github.com>
2023-11-28 12:50:32 +0530

    mongodb changes

--- backend/todolist.js
+++ backend/todolist.js
@@ -5,29 +5,41 @@ const dbURI = "mongodb+srv://admin:rpdiVsK3tLnbFrfH@cluster0.a4ifhlu.mongodb.net
 
 const { MongoClient } = require('mongodb');
 
-const uri = "your-mongodb-connection-string"; 
+const uri = "your-mongodb-connection-string";
 const client = new MongoClient(dbURI);
-const database = client.db('todolist'); 
+const database = client.db('todolist');
 run().catch(console.dir);
 
 app.use(cors());
 
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.post('/signup', async (req, res) => {
-  const collection = database.collection('users'); 
+  const collection = database.collection('users');
 
   try {
     const data = req.body;
     const jsonString = Object.keys(data)[0];
     const parsedData = JSON.parse(jsonString);
 
-    const existingUser = await collection.findOne({ email: parsedData.email });
-    if (existingUser) {
+    const aggregation = [
+      {
+        $match: {
+          email: parsedData.email
+        }
+      },
+      {
+        $limit: 1
+      }
+    ];
+
+    const existingUsers = await collection.aggregate(aggregation).toArray();
+
+    if (existingUsers.length > 0) {
       return res.status(400).send('User already exists');
     }
 
@@ -35,36 +47,45 @@ app.post('/signup', async (req, res) => {
 
     res.status(201).send({ status: true, message: 'User created successfully, Please Login!!', userId: result.insertedId });
   } catch (error) {
-    res.status(201).send({ status: false, message: 'Error creating user' });
-
+    console.error(error);
+    res.status(500).send({ status: false, message: 'Error creating user' });
   }
 });
 
+
 app.post('/login', async (req, res) => {
-  const collection = database.collection('users'); 
+  const collection = database.collection('users');
 
   try {
-
     const data = req.body;
-
     const jsonString = Object.keys(data)[0];
     const parsedData = JSON.parse(jsonString);
-    const existingUser = await collection.findOne({ email: parsedData.email });
-    // console.log(existingUser)
 
-    if (!existingUser) {
+    const aggregation = [
+      {
+        $match: {
+          email: parsedData.email
+        }
+      },
+      {
+        $limit: 1
+      }
+    ];
+
+    const users = await collection.aggregate(aggregation).toArray();
+
+    if (users.length === 0) {
       return res.status(400).send({ status: false, message: 'Invalid credentials' });
-
     } else {
-      return res.status(200).send({ status: true, message: 'User loggedin' });
-
+      return res.status(200).send({ status: true, message: 'User logged in' });
     }
 
   } catch (error) {
-    res.status(201).send({ status: false, message: 'Error creating user' });
-
+    console.error(error);
+    res.status(500).send({ status: false, message: 'Error logging in user' });
   }
 });
+
 
 
 async function run() {
